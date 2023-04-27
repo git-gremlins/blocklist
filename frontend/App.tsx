db772571dbc33c04fe926e147c575e2d424f7674
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-04-27 12:42:10 +0100

    Setup environment variables import

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { CLERK_PUBLISHABLE_KEY } from "@env";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider publishableKey="pk_test_ZGlzY3JldGUtY3JheWZpc2gtMzcuY2xlcmsuYWNjb3VudHMuZGV2JA">
+      <View style={styles.container}>
+        <Text>
+          Open up App.tsx to start {CLERK_PUBLISHABLE_KEY} working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ frontend/types/env.d.tsx
@@ -0,0 +1,3 @@
+declare module "@env" {
+  export const CLERK_PUBLISHABLE_KEY: string;
+}
