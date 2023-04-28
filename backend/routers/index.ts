d5ff7d0c1ef221fd8a4fedbb26024b2dd26342f5
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-04-28 13:44:13 +0100

    cleanup tasks to use controller

--- backend/routers/index.ts
+++ backend/routers/index.ts
@@ -1,5 +1,6 @@
-import { tasksRouter } from "./tasks";
 import { router } from "../utils/trpc";
+import tasksRouter from "./tasks";
+import usersRouter from "./users";
 
 export const appRouter = router({
   tasks: tasksRouter,
