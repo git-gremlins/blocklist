76599ccd1860f7de07af873d966d7383f9af7f86
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-04-28 16:10:49 +0100

    add base tests for routes

---
+++ backend/routers/__test__/taskRoutes.test.ts
@@ -0,0 +1,28 @@
+import z from "zod";
+import trpcClient from "../../utils/trpc/trpsTestClient";
+
+describe("getTasks", () => {
+  it("should return an array", async () => {
+    const tasks = await trpcClient.tasks.getTasks.query();
+    expect(Array.isArray(tasks)).toBe(true);
+  });
+  it("returned array should have length of 20", async () => {
+    const tasks = await trpcClient.tasks.getTasks.query();
+    expect(tasks).toHaveLength(20);
+  });
+  it("Each task array element should match our object type of task", async () => {
+    const zTask = z.object({
+      taskId: z.number(),
+      name: z.string(),
+      descriptions: z.string(),
+      parentTaskId: z.number().nullable(),
+      userId: z.number(),
+      importance: z.string(),
+      deadline: z.date().nullable(),
+    });
+    const tasks = await trpcClient.tasks.getTasks.query();
+    tasks.map((task) => {
+      expect(zTask.safeParse(task)).not.toBeUndefined();
+    });
+  });
+});
