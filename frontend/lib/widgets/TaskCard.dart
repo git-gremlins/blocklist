c606b0eee73cbeecd83de478d5d79c32c4ff774c
JoeB-G <joeboddenglennon@hotmail.co.uk>
2023-05-09 12:38:22 +0100

    refactor pop up task update

--- frontend/lib/widgets/TaskCard.dart
+++ frontend/lib/widgets/TaskCard.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/api.dart';
 import 'package:frontend/screens/sub_tasks.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:frontend/types/task/delete_task.dart';
-import 'package:frontend/types/task/update_task.dart';
+import 'package:frontend/widgets/Update_task_form.dart';
 
 import '../helpers/colour_choice.dart';
 
@@ -20,94 +18,6 @@ class TaskCard extends StatefulWidget {
 }
 
 class _TaskCardState extends State<TaskCard> {
-  late TextEditingController _titleController;
-  late TextEditingController _descriptionController;
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  void initState() {
-    super.initState();
-    _titleController = TextEditingController(text: widget.task["name"]);
-    _descriptionController =
-        TextEditingController(text: widget.task["description"]);
-  }
-
-  @override
-  void dispose() {
-    _titleController.dispose();
-    _descriptionController.dispose();
-    super.dispose();
-  }
-
-  void _submitForm() async {
-    if (_formKey.currentState!.validate()) {
-      Navigator.pop(context);
-      updateTask(UpdateTask(
-          taskId: widget.task["taskId"],
-          name: _titleController.text,
-          description: _descriptionController.text));
-    }
-  }
-
-  Widget _editTitleTextField() {
-    return AlertDialog(
-      title: const Text('Edit your task'),
-      content: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextFormField(
-              controller: _titleController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'change task name';
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                labelText: 'Task',
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              controller: _descriptionController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'change your description';
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                labelText: 'Description',
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () async {
-                Navigator.pop(context);
-                await deleteTask(RemoveTask(taskId: widget.task["taskId"]));
-              },
-              child: const Text('Delete Task'),
-            )
-          ],
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Cancel'),
-        ),
-        ElevatedButton(
-          onPressed: _submitForm,
-          child: const Text('Submit'),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -124,7 +34,7 @@ class _TaskCardState extends State<TaskCard> {
         showDialog(
           context: context,
           builder: ((context) {
-            return _editTitleTextField();
+            return UpdateTaskForm(task: widget.task);
           }),
         );
       },
