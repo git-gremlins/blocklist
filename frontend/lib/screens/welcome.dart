aabe4114df65f77246df9ddf7904b11056e137d5
cherrydub <cherrydub@gmail.com>
2023-05-09 16:58:46 +0100

    welcome page icon updated, instructions page added

--- frontend/lib/screens/welcome.dart
+++ frontend/lib/screens/welcome.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:frontend/screens/splash_screen.dart';
 
 class Welcome extends StatelessWidget {
   static const List<String> backgroundImages = [
-    'https://i.seadn.io/gae/0jPQNCjX-zkP8NjDnlmOTlQsrgE5mO8o4m4Seotr6JSFr4SLeRZilyp_dB5K430x59PiNe6xT37Docg7jZ1Xeaf-1GIMcvBrc2Qmeg?auto=format&w=1000',
+    // 'https://i.seadn.io/gae/0jPQNCjX-zkP8NjDnlmOTlQsrgE5mO8o4m4Seotr6JSFr4SLeRZilyp_dB5K430x59PiNe6xT37Docg7jZ1Xeaf-1GIMcvBrc2Qmeg?auto=format&w=1000',
     'https://i.seadn.io/gae/w5XoKsN9vXLP0_cKIPM4bB5cJSGfgKt241MMl1FxD4IhcnTlzkhBN3HAVVhSqCfib_3_iUB0t_tTzhak-hlQ_oOVv0frCMkJFlbKwQ?auto=format&w=1000',
     'https://i.seadn.io/gae/by84lEwLcPErvS8_E3Sarerepmi74PBp85fxeHTor7xadp5-bT20pOHm2bsffNAsJmSRnJdsf7T_m5PGVSBSiv2nkllEoMK3t167gw?auto=format&w=1000',
     'https://i.seadn.io/gae/tbdHR04RZZWqu2ZZoRZBE65dtw3DNZrjcb2j_MOTES0fH3eDfW1D8XiFzKcFbMVx6tribNOp5euWUPl1YWeNnWLGtG0gBlAbDExfnz0?auto=format&w=1000',
@@ -31,7 +32,7 @@ class Welcome extends StatelessWidget {
               child: Text(
                 "Welcome to BlockList",
                 style: TextStyle(
-                  fontSize: 55,
+                  fontSize: 60,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),
@@ -42,9 +43,12 @@ class Welcome extends StatelessWidget {
               right: 20,
               child: IconButton(
                 onPressed: () {
-                  Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SplashPage()),
+                  );
                 },
-                icon: Icon(Icons.arrow_forward),
+                icon: Icon(Icons.login),
                 color: Colors.white.withOpacity(0.8),
               ),
             ),
