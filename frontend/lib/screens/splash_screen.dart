23d373a62a5081537e871b4ca2e1db49cad6d81e
cherrydub <cherrydub@gmail.com>
2023-05-10 00:28:56 +0100

    minor changes for this branch

--- frontend/lib/screens/splash_screen.dart
+++ frontend/lib/screens/splash_screen.dart
@@ -26,11 +26,8 @@ class _SplashPageState extends State<SplashPage> {
   }
 
   static const List<String> backgroundImages = [
-    'https://i.seadn.io/gae/0jPQNCjX-zkP8NjDnlmOTlQsrgE5mO8o4m4Seotr6JSFr4SLeRZilyp_dB5K430x59PiNe6xT37Docg7jZ1Xeaf-1GIMcvBrc2Qmeg?auto=format&w=1000',
-    'https://i.seadn.io/gae/w5XoKsN9vXLP0_cKIPM4bB5cJSGfgKt241MMl1FxD4IhcnTlzkhBN3HAVVhSqCfib_3_iUB0t_tTzhak-hlQ_oOVv0frCMkJFlbKwQ?auto=format&w=1000',
-    'https://i.seadn.io/gae/by84lEwLcPErvS8_E3Sarerepmi74PBp85fxeHTor7xadp5-bT20pOHm2bsffNAsJmSRnJdsf7T_m5PGVSBSiv2nkllEoMK3t167gw?auto=format&w=1000',
-    'https://i.seadn.io/gae/tbdHR04RZZWqu2ZZoRZBE65dtw3DNZrjcb2j_MOTES0fH3eDfW1D8XiFzKcFbMVx6tribNOp5euWUPl1YWeNnWLGtG0gBlAbDExfnz0?auto=format&w=1000',
-    'https://i.seadn.io/gcs/files/6fdd0cb3b4dddccaa7ff95e2d5f91429.jpg?auto=format&w=1000'
+    // 'https://i.seadn.io/gae/0jPQNCjX-zkP8NjDnlmOTlQsrgE5mO8o4m4Seotr6JSFr4SLeRZilyp_dB5K430x59PiNe6xT37Docg7jZ1Xeaf-1GIMcvBrc2Qmeg?auto=format&w=1000',
+    'https://i.seadn.io/gae/w5XoKsN9vXLP0_cKIPM4bB5cJSGfgKt241MMl1FxD4IhcnTlzkhBN3HAVVhSqCfib_3_iUB0t_tTzhak-hlQ_oOVv0frCMkJFlbKwQ?auto=format&w=1200',
   ];
 
   String _getRandomBackgroundImage() {
