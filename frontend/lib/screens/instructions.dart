0ccc02d0c63feff5ceb51aa2f722e504bc298229
cherrydub <cherrydub@gmail.com>
2023-05-09 16:58:46 +0100

    welcome page icon updated, instructions page added

--- frontend/lib/screens/instructions.dart
+++ frontend/lib/screens/instructions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'dart:math';
 
 class Instructions extends StatelessWidget {
@@ -12,6 +13,7 @@ class Instructions extends StatelessWidget {
 
   String _getRandomBackgroundImage() {
     final random = Random();
+
     return backgroundImages[random.nextInt(backgroundImages.length)];
   }
 
@@ -20,36 +22,106 @@ class Instructions extends StatelessWidget {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image: NetworkImage(
-                  _getRandomBackgroundImage(),
-                ),
-                fit: BoxFit.cover)),
-        child: Stack(
+          image: DecorationImage(
+            image: NetworkImage(_getRandomBackgroundImage()),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
+            Padding(
+              padding: const EdgeInsets.only(left: 20, top: 50, bottom: 10),
               child: Text(
-                "Instructions",
+                "INSTRUCTIONS:",
                 style: TextStyle(
-                  fontSize: 36,
+                  fontSize: 28,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white,
+                  color: Colors.white.withOpacity(0.8),
                 ),
               ),
             ),
-            Positioned(
-              bottom: 20,
-              left: 20,
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                children: [
+                  _buildInstructionContainer(
+                    "GENERAL INFO",
+                    "Blocklist is a to-do app with interactive tasks, your base page will have a white background. If you are within a subtask, the background color will reflect which task you are within",
+                  ),
+                  SizedBox(height: 20),
+                  _buildInstructionContainer(
+                    "NEW TASK",
+                    "The grid is 4 x 8 containing 32 squares. click on any empty square, hold and drag to the desired dimensions. As long as there are no other tasks in the way, a new rectangle/square will appear",
+                  ),
+                  SizedBox(height: 20),
+                  _buildInstructionContainer(
+                    "EDIT TASK",
+                    "Double click on task to update task title and description. You can also delete the task from here",
+                  ),
+                  SizedBox(height: 20),
+                  _buildInstructionContainer(
+                    "DELETE TASK",
+                    "You can either double click to edit the task and delete. Alternatively you can click the task and locate the delete/bin icon bottom right",
+                  ),
+                  SizedBox(height: 20),
+                  _buildInstructionContainer(
+                    "MOVE TASK",
+                    "Press and hold on your current task. You will see a border appear, you can now drag this task to any open grid.",
+                  ),
+                  SizedBox(height: 20),
+                  _buildInstructionContainer(
+                    "LOG OUT",
+                    "On the main grid there will be a logout button on the bottom right",
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20, left: 20),
               child: IconButton(
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
-                icon: Icon(Icons.arrow_back),
+                icon: Row(
+                  children: [
+                    Icon(Icons.arrow_back),
+
+                    // Icon(Icons.login),
+                  ],
+                ),
                 color: Colors.white.withOpacity(0.8),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildInstructionContainer(String title, String text) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.7),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            text,
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
       ),
     );
   }
